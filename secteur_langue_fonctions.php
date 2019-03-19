b4c9f469f5be8b51e76fea0fd38779da41b5e98a
Rainer <rainer@websimple.be>
2019-03-19 14:40:40 +0100

    fonction repris de trad_rub

--- secteur_langue_fonctions.php
+++ secteur_langue_fonctions.php
@@ -13,11 +13,66 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
-
-/*
- * Un fichier de fonctions permet de définir des éléments
- * systématiquement chargés lors du calcul des squelettes.
+/**
+ * Détermine l'id_parent de la nouvell rubrique traduite
  *
- * Il peut par exemple définir des filtres, critères, balises, …
- * 
- */
+ * @param string $lang
+ *   Langue de destination
+ * @param string $id_trad
+ *   L'id de traduction
+ * @param bool $creer_racine
+ *   si true, il faut créer la racine
+ *
+ * @return array
+ *   un tablea avec Langue de destination, L'id de traduction et le bool creer_racine
+  */
+
+
+
+function rubrique_destination_traduction($lang, $id_trad, $creer_racine = '') {
+	$id_trad_parent = '';
+	$trads = [];
+
+	if ($lang AND $id_trad) {
+		// on établit l'id_parent
+		$id_trad_parent = sql_getfetsel('id_parent', 'spip_rubriques', 'id_rubrique=' . $id_trad);
+
+		//puis sa traduction
+		if ($id_trad_parent)
+			$id_parent_trad = sql_getfetsel('id_trad', 'spip_rubriques', 'id_rubrique=' . $id_trad_parent);
+		if ($id_trad_parent == 0)
+			$trads = [
+				0 => 0,
+				1 => $id_trad,
+				2 => ''
+			];
+		// S'il il existe une traduction parente dans la langue demandé on retourne son id
+		elseif ($id_parent_trad) {
+			$rub = sql_fetsel('id_rubrique,id_trad', 'spip_rubriques', 'id_trad=' . $id_parent_trad . ' AND lang=' . sql_quote($lang));
+			if ($rub) {
+				$trads = [
+					0 => $rub['id_rubrique'],
+					1 => $id_trad,
+					2 => $creer_racine
+				];
+			}
+			else {
+				$id_trad = sql_getfetsel('id_trad', 'spip_rubriques', 'id_trad=' . $id_parent_trad);
+				$trads = rubrique_destination_traduction($lang, $id_trad, 'oui');
+			}
+		}
+		elseif ($id_trad_parent) {
+			$trads = rubrique_destination_traduction($lang, $id_trad_parent, 'oui');
+		}
+		else
+			$trads = [
+				0 => 0,
+				1 => $id_trad
+			];
+
+		return $trads;
+
+	}
+	return $trads;
+
+}
